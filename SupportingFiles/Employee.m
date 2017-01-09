57873318eecec0395965e8d44c4aab51d108ef6e
Stuart Pineo <svpineo@gmail.com>
2017-01-09 09:18:54 -0500

    Rename dirs/files.

---
+++ SupportingFiles/Employee.m
@@ -0,0 +1,18 @@
+//
+//  Employee.m
+//  
+//
+//  Created by Stuart Pineo on 12/31/15.
+//
+//
+
+#import "Employee.h"
+#import "DepEmployee.h"
+
+
+@implementation Employee
+
+@dynamic name;
+@dynamic employee_de;
+
+@end
